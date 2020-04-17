4b6164dc34812db486e1e8b6e85a4559906c5b77
Kevin Nguyen <60084108+Kvnvn@users.noreply.github.com>
2020-04-17 00:39:12 -0700

    add getALL method to orm

--- config/orm.js
+++ config/orm.js
@@ -9,4 +9,10 @@ const orm = {
 }
 module.exports = orm;
 
-// orm.getAll("todos",todos=>console.log(todos))
+orm.getAll("burgers",burgers=>console.log(burgers));
+
+// [
+// RowDataPacket { id: 1, burger_name: 'DOUBLE-DOUBLE', devoured: 1 },
+// RowDataPacket { id: 2, burger_name: 'Cheese-burger', devoured: 0 },
+// RowDataPacket { id: 3, burger_name: 'Veggie-Burger', devoured: 0 }
+// ]
