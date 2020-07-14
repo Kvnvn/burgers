5713cd0b45b3d9fd958da13b83e6feb98acaf8f1
Kevin Nguyen <60084108+Kvnvn@users.noreply.github.com>
2020-07-14 14:48:04 -0700

    added ajax call to index.handlebars

--- server.js
+++ server.js
@@ -2,20 +2,20 @@ const express = require("express");
 const exphbs = require("express-handlebars");
 const homecontroller = require("./controllers/homeControllers");
 
-
 const app = express();
 
 const PORT = process.env.PORT || 8080;
+app.use(express.static("public"));
 
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-app.use(express.static( "/public", { extensions: ["css", "js"] }));
 
 
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
 app.use(homecontroller)
 
 
