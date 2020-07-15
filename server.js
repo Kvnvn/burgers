08d208496d16cb5a322b5d94616006b6da911927
Kevin Nguyen <60084108+Kvnvn@users.noreply.github.com>
2020-07-14 22:15:35 -0700

    added in images and changed up look of app

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const path = require("path");
 const express = require("express");
 const exphbs = require("express-handlebars");
 const homecontroller = require("./controllers/homeControllers");
@@ -5,13 +6,11 @@ const homecontroller = require("./controllers/homeControllers");
 const app = express();
 
 const PORT = process.env.PORT || 8080;
+
 app.use(express.static("public"));
 
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
-
-
-
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
